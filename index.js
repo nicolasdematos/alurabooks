16caa750336cfd32383e98cf562edc05e556b793
Nicolas Matos <112784558+nicolasdematos@users.noreply.github.com>
2022-09-17 22:59:56 -0300

    header

---
+++ index.js
@@ -0,0 +1,7 @@
+function showhide() {
+    console.log(document.querySelector(".menu"))
+    menu = document.querySelector(".menu")
+    if (menu.style.display == 'none') {
+        console.log("aaaa")
+    }
+}
