144281c151644989473ab3b4242a5a5609dfd95a
Nicolas Matos <112784558+nicolasdematos@users.noreply.github.com>
2022-09-22 21:14:23 -0300

    changes

--- index.js
+++ index.js
@@ -6,3 +6,13 @@ function showhide() {
         document.querySelector(".menu").style.display = 'block'
     }
 }
+
+function hideshowcontent() {
+    console.log('teste')
+    if (document.querySelector(".menu2").style.display == 'block') {
+        document.querySelector(".menu2").style.display = 'none'
+        document.querySelector(".hideshowcontent_button").style.display
+    } else {
+        document.querySelector(".menu2").style.display = 'block'
+    }
+}
